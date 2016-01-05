53f0e3ed71eb33f517a53cabb71be421452e1394
Daniele Santoro <psyacca@gmail.com>
2016-01-06 00:47:49 +0100

    add persistence link to JBoss datasource on the backend application

---
+++ grocery-backend/src/main/java/it/unitn/dsantoro/a9/model/User.java
@@ -0,0 +1,25 @@
+package it.unitn.dsantoro.a9.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+
+public class User implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	
+	public User() {
+		super();
+	}
+   
+}
